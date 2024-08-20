48aa036d11cd52bb90d3a0393d6ab32f8b8c81b3
thuanptt2 <thuanptt2@gmail.com>
2024-08-20 17:30:20 +0700

    Config Flexible Database Connection

--- TechShopSolution.Infrastructure/Migrations/20240820102450_ChangeTableName.Designer.cs
+++ TechShopSolution.Infrastructure/Migrations/20240820102450_ChangeTableName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechShopSolution.Infrastructure.DBContext;
 
@@ -11,9 +12,11 @@ using TechShopSolution.Infrastructure.DBContext;
 namespace TechShopSolution.Infrastructure.Migrations
 {
     [DbContext(typeof(TechShopDbContext))]
-    partial class TechShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240820102450_ChangeTableName")]
+    partial class ChangeTableName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TechShopSolution.Infrastructure/Migrations/20240820102450_ChangeTableName.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechShopSolution.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MetaTittle",
+                table: "Product",
+                newName: "MetaTitle");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CodPrice",
+                table: "Transport",
+                type: "MONEY",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MetaTitle",
+                table: "Product",
+                newName: "MetaTittle");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CodPrice",
+                table: "Transport",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "MONEY");
+        }
+    }
+}
